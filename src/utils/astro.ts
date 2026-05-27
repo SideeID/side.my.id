0d2ab2eea3e79217ae37b96f9575ceac68f92df4
SideeID <sandipratama164026@gmail.com>
2026-05-27 18:34:39 +0700

    fix: update dependencies and add new notes on RAG concepts

--- src/utils/astro.ts
+++ src/utils/astro.ts
@@ -1,17 +1,19 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import compressor from "astro-compressor";
-import { envField } from "astro/config";
-import rehypePresetMinify from "rehype-preset-minify";
-import rehypeSlug from "rehype-slug";
-import remarkSectionize from "remark-sectionize";
-import remarkToc from "remark-toc";
-import { remarkReadingTime } from "../../remark-reading-time.mjs";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import compressor from 'astro-compressor';
+import { envField } from 'astro/config';
+import rehypePresetMinify from 'rehype-preset-minify';
+import rehypeSlug from 'rehype-slug';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import remarkSectionize from 'remark-sectionize';
+import remarkToc from 'remark-toc';
+import { remarkReadingTime } from '../../remark-reading-time.mjs';
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
-import sentry from "@sentry/astro";
-import { SENTRY_AUTH_TOKEN, SENTRY_DSN, SENTRY_PROJECT } from "./constants";
+import sentry from '@sentry/astro';
+import { SENTRY_AUTH_TOKEN, SENTRY_DSN, SENTRY_PROJECT } from './constants';
 
 export const integrations = [
   mdx({
@@ -20,11 +22,12 @@ export const integrations = [
       theme: 'dracula-soft',
     },
     remarkPlugins: [
+      remarkMath,
       [remarkToc, { heading: 'toc' }],
       remarkReadingTime,
       remarkSectionize,
     ],
-    rehypePlugins: [rehypePresetMinify, rehypeSlug],
+    rehypePlugins: [rehypeKatex, rehypePresetMinify, rehypeSlug],
     remarkRehype: {
       footnoteLabel: 'Footnotes',
     },
@@ -54,32 +57,32 @@ export const integrations = [
 export const env = {
   schema: {
     PUBLIC_PRODUCTION_URL: envField.string({
-      context: "client",
-      access: "public",
+      context: 'client',
+      access: 'public',
     }),
     PUBLIC_DEVELOPMENT_URL: envField.string({
-      context: "client",
-      access: "public",
+      context: 'client',
+      access: 'public',
     }),
     TURSO_DATABASE_URL: envField.string({
-      context: "server",
-      access: "secret",
+      context: 'server',
+      access: 'secret',
     }),
     TURSO_AUTH_TOKEN: envField.string({
-      context: "server",
-      access: "secret",
+      context: 'server',
+      access: 'secret',
     }),
     SENTRY_DSN: envField.string({
-      context: "server",
-      access: "secret",
+      context: 'server',
+      access: 'secret',
     }),
     SENTRY_AUTH_TOKEN: envField.string({
-      context: "server",
-      access: "secret",
+      context: 'server',
+      access: 'secret',
     }),
     SENTRY_PROJECT: envField.string({
-      context: "server",
-      access: "secret",
+      context: 'server',
+      access: 'secret',
     }),
   },
 };
